a2f0d05fd35d141e1ddcc552dc9e71dfef6fa9fc
Emir Al <emir.al@havas.com>
2023-05-10 11:58:31 +0300

    starter

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,58 +1,40 @@
-import { OrbitControls } from '@react-three/drei';
-import { button, useControls } from 'leva';
-import { Perf } from 'r3f-perf';
+import { useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { useRef } from 'react'
+import { Perf } from 'r3f-perf'
 
-export default function Experience() {
-  const { perfVisible } = useControls('perf', { perfVisible: true });
-  const { position, color, visible } = useControls('sphere', {
-    position: {
-      value: { x: -2, y: 0 },
-      step: 0.01,
-      joystick: 'invertY',
-    },
-    color: '#ff0000',
-    visible: true,
-    clickMe: button(() => {
-      console.log('ok');
-    }),
-    choice: { options: ['a', 'b', 'c'] },
-  });
-  const { position2, color2, visible2 } = useControls('cube', {
-    position2: {
-      value: { x: -2, y: 0 },
-      step: 0.01,
-      joystick: 'invertY',
-    },
-    color2: '#ff0000',
-    visible2: true,
-    clickMe: button(() => {
-      console.log('ok');
-    }),
-    choice: { options: ['a', 'b', 'c'] },
-  });
+export default function Experience()
+{
+    const cube = useRef()
+    
+    useFrame((state, delta) =>
+    {
+        cube.current.rotation.y += delta * 0.2
+    })
 
-  return (
-    <>
-      {perfVisible ? <Perf position='top-left' /> : null}
-      <OrbitControls makeDefault />
+    return <>
 
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
-      <ambientLight intensity={0.5} />
+        <Perf position="top-left" />
 
-      <mesh position={[position.x, position.y, 0]} visible={visible}>
-        <sphereGeometry />
-        <meshStandardMaterial color={color} />
-      </mesh>
+        <OrbitControls makeDefault />
 
-      <mesh position-x={2} scale={1.5}>
-        <boxGeometry />
-        <meshStandardMaterial color='mediumpurple' />
-      </mesh>
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
 
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
-        <planeGeometry />
-        <meshStandardMaterial color='greenyellow' />
-      </mesh>
+        <mesh position-x={ - 2 }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
+
+        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
     </>
-  );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,19 @@
-import './style.css';
-import ReactDOM from 'react-dom/client';
-import { Canvas } from '@react-three/fiber';
-import Experience from './Experience.jsx';
-import { Leva } from 'leva';
+import './style.css'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience.jsx'
 
-const root = ReactDOM.createRoot(document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-  <>
-    <Leva collapsed />
     <Canvas
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [-4, 3, 6],
-      }}
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [ - 4, 3, 6 ]
+        } }
     >
-      <Experience />
+        <Experience />
     </Canvas>
-  </>
-);
+)
