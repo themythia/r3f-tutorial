951e6496e054a5c53b6364b5375297119b52b37c
Emir Al <emir.al@havas.com>
2023-05-11 10:12:46 +0300

    starter

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,24 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import Experience from './Experience.jsx'
+import './style.css';
+import ReactDOM from 'react-dom/client';
+import { Canvas } from '@react-three/fiber';
+import Experience from './Experience.jsx';
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+const created = ({ gl }) => {
+  gl.setClearColor('#ff0000', 1);
+};
 
 root.render(
-    <Canvas
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [ - 4, 3, 6 ]
-        } }
-    >
-        <Experience />
-    </Canvas>
-)
+  <Canvas
+    camera={{
+      fov: 45,
+      near: 0.1,
+      far: 200,
+      position: [-4, 3, 6],
+    }}
+    onCreated={created}
+  >
+    <Experience />
+  </Canvas>
+);
