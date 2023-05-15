c3901ec785f6cb90fe1d24136a8e96664e800bc4
Emir Al <emir.al@havas.com>
2023-05-15 10:18:56 +0300

    3d text

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,19 @@
-import './style.css';
-import ReactDOM from 'react-dom/client';
-import { Canvas } from '@react-three/fiber';
-import Experience from './Experience.jsx';
+import './style.css'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience.jsx'
 
-const root = ReactDOM.createRoot(document.querySelector('#root'));
-
-const created = ({ gl }) => {
-  gl.setClearColor('#ff0000', 1);
-};
+const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-  <Canvas
-    camera={{
-      fov: 45,
-      near: 0.1,
-      far: 200,
-      position: [-4, 3, 6],
-    }}
-    onCreated={created}
-  >
-    <Experience />
-  </Canvas>
-);
+    <Canvas
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [ 4, - 2, 6 ]
+        } }
+    >
+        <Experience />
+    </Canvas>
+)
