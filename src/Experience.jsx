b5d91f28a2b87e56377695e81cfe1c209498aa46
Emir Al <emir.al@havas.com>
2023-05-16 09:17:49 +0300

    starter

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,75 +1,37 @@
-import {
-  Sparkles,
-  Center,
-  useGLTF,
-  useTexture,
-  OrbitControls,
-  shaderMaterial,
-} from '@react-three/drei';
-import portalVertexShader from './shaders/portal/vertex.glsl';
-import portalFragmentShader from './shaders/portal/fragment.glsl';
-import * as THREE from 'three';
-import { extend, useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { useRef } from 'react'
 
-const PortalMaterial = shaderMaterial(
-  {
-    uTime: 0,
-    uColorStart: new THREE.Color('#ffffff'),
-    uColorEnd: new THREE.Color('#000000'),
-  },
-  portalVertexShader,
-  portalFragmentShader
-);
+export default function Experience()
+{
+    const cube = useRef()
+    
+    useFrame((state, delta) =>
+    {
+        cube.current.rotation.y += delta * 0.2
+    })
 
-extend({ PortalMaterial });
+    return <>
 
-export default function Experience() {
-  const { nodes } = useGLTF('./model/portal.glb');
-  const bakedTexture = useTexture('./model/baked.jpg');
-  bakedTexture.flipY = false;
+        <OrbitControls makeDefault />
 
-  const portalMaterial = useRef();
-  useFrame((state, delta) => {
-    portalMaterial.current.uTime += delta;
-  });
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
 
-  return (
-    <>
-      <color args={['#030202']} attach='background' />
-
-      <OrbitControls makeDefault />
-      <Center>
-        <mesh geometry={nodes.baked.geometry}>
-          <meshBasicMaterial map={bakedTexture} />
+        <mesh position-x={ - 2 }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
         </mesh>
-        <mesh
-          geometry={nodes.poleLightA.geometry}
-          position={nodes.poleLightA.position}
-        >
-          <meshBasicMaterial color='#ffffe5' />
+
+        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
         </mesh>
-        <mesh
-          geometry={nodes.poleLightB.geometry}
-          position={nodes.poleLightB.position}
-        >
-          <meshBasicMaterial color='#ffffe5' />
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
         </mesh>
-        <mesh
-          geometry={nodes.portalLight.geometry}
-          position={nodes.portalLight.position}
-          rotation={nodes.portalLight.rotation}
-        >
-          <portalMaterial ref={portalMaterial} side={THREE.DoubleSide} />
-        </mesh>
-        <Sparkles
-          size={6}
-          scale={[4, 2, 4]}
-          position-y={1}
-          speed={0.2}
-          count={40}
-        />
-      </Center>
+
     </>
-  );
 }
