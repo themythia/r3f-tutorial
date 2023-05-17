441a12394fd50f068bca63a34885bf406dd125d5
Emir Al <emir.al@havas.com>
2023-05-17 10:12:03 +0300

    post processing

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,31 +1,55 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls } from '@react-three/drei';
+import { Perf } from 'r3f-perf';
+import {
+  Vignette,
+  EffectComposer,
+  Glitch,
+  Bloom,
+} from '@react-three/postprocessing';
+import { BlendFunction } from 'postprocessing';
+export default function Experience() {
+  return (
+    <>
+      <color attach='background' args={['#000000']} />
+      <EffectComposer multisampling={0}>
+        {/* <Vignette
+          offset={0.3}
+          darkness={0.9}
+          blendFunction={BlendFunction.NORMAL}
+        /> */}
+        {/* <Bloom mipmapBlur /> */}
+      </EffectComposer>
+      <Perf position='top-left' />
 
-export default function Experience()
-{
-    return <>
+      <OrbitControls makeDefault />
 
-        <Perf position="top-left" />
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
 
-        <OrbitControls makeDefault />
+      <mesh castShadow position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial
+          color='orange'
+          emissive='orange'
+          emissiveIntensity={1}
+          toneMapped={false}
+        />
+      </mesh>
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+      <mesh castShadow position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color={[1, 1.4, 4]} toneMapped={false} />
+      </mesh>
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
+      <mesh
+        receiveShadow
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+      >
+        <planeGeometry />
+        <meshStandardMaterial color='greenyellow' />
+      </mesh>
     </>
+  );
 }
